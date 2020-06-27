4f587ca41d3ef442c91d23bedab7b569048a9567
KlaraGrant <67516412+KlaraGrant@users.noreply.github.com>
2020-06-27 19:32:13 +0200

    Website

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./"))
+	http.Handle("/", fs)
+
+	log.Println("Listening on :80...")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// 25368
